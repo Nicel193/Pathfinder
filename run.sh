cee2fe5bfc9a6cb2c4de62d29fa216bdade020aa
Nicel193 <nicelgoog@gmail.com>
2023-01-06 12:25:53 +0200

    update tests

--- run.sh
+++ run.sh
@@ -2,7 +2,13 @@ make all
 
 zsh run_test.sh easy
 zsh run_test.sh hard
+zsh run_test.sh hardest
 zsh run_test.sh medium
 zsh run_test.sh superhard
+zsh run_test.sh empty
+zsh run_test.sh invalid1
+zsh run_test.sh invalid2
+zsh run_test.sh invalid3
+zsh run_test.sh invalid4
 
 make uninstall
