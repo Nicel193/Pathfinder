79bc75739607fa5f498a962ba19fa1d8e5cec082
root <root@hp.>
2024-10-26 21:47:08 +0000

    Refactor pathfinder

--- src/pathfinder.c
+++ src/pathfinder.c
@@ -1,5 +1,11 @@
 #include "../inc/pathfinder.h"
 
+void cleanup_resources(t_list *names_verticesdup, int **graph, int count_vertices)
+{
+    delete_list(names_verticesdup);
+    delete_matrix(graph, count_vertices);
+}
+
 void pathfinder(t_list *nodes, t_list *names_vertices, int count_vertices)
 {
     int **graph = create_matrix(nodes, names_vertices, count_vertices);
@@ -7,20 +13,17 @@ void pathfinder(t_list *nodes, t_list *names_vertices, int count_vertices)
 
     t_list *temp = names_verticesdup;
     int n = count_vertices;
-    
+
     while (n != 1)
     {
-        // print_matrix(graph, count_vertices);
         find_pathway(names_vertices, graph, count_vertices, n - 1);
-
         mx_swap_list(names_vertices, temp->data, temp->next->data);
-        temp = temp->next;
 
         graph = matrix_rebild(graph, nodes, names_vertices, count_vertices);
+
+        temp = temp->next;
         n--;
     }
 
-    delete_list(names_verticesdup);
-    delete_matrix(graph, count_vertices);
+    cleanup_resources(names_verticesdup, graph, count_vertices);
 }
-
--- src/print_path.c
+++ src/print_path.c
@@ -1,40 +1,45 @@
 #include "../inc/pathfinder.h"
 
-void print_path(t_part_path path[], char *last_element_name, int total_distance)
-{
+void print_separator() {
     mx_printstr("========================================\n");
+}
+
+void print_path_header(t_part_path path[], char *last_element_name) {
     mx_printstr("Path: ");
     mx_printstr(path[0].name);
     mx_printstr(" -> ");
     mx_printstr(last_element_name);
     mx_printchar('\n');
+}
 
+void print_route(t_part_path path[]) {
     mx_printstr("Route: ");
-    for (int i = 0; path[i].name != NULL; i++)
-    {
+    for (int i = 0; path[i].name != NULL; i++) {
         mx_printstr(path[i].name);
-
         if (path[i + 1].name != NULL)
             mx_printstr(" -> ");
     }
-
     mx_printchar('\n');
-    mx_printstr("Distance: ");
-    if (path[2].name != NULL)
-    {
-        for (int i = 1; path[i].name != NULL; i++)
-        {
-            mx_printint(path[i].length);
+}
 
+void print_distance(t_part_path path[], int total_distance) {
+    mx_printstr("Distance: ");
+    if (path[2].name != NULL) {
+        for (int i = 1; path[i].name != NULL; i++) {
+            mx_printint(path[i].length);
             if (path[i + 1].name != NULL)
                 mx_printstr(" + ");
         }
-
         mx_printstr(" = ");
     }
     mx_printint(total_distance);
     mx_printchar('\n');
-
-    mx_printstr("========================================\n");
 }
 
+void print_path(t_part_path path[], char *last_element_name, int total_distance) {
+    print_separator();
+    print_path_header(path, last_element_name);
+    print_route(path);
+    print_distance(path, total_distance);
+    print_separator();
+}
